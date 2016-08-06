8f85a6dc728e667c1a04831530e9b5e021a2b4d9
Nicolás Castano <nicolas.castano@quantumfactory.com.ar>
2016-08-06 14:02:17 -0300

    Estructura + inicio pantalla login

---
+++ app/components/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app',
+  templateUrl: 'app/templates/login.html',
+  styleUrls: ['content/styles.css']
+})
+export class AppComponent { }
